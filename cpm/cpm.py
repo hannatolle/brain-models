83e28cded2d97edd1245e82ece910004228795b4
Hanna Tolle <hanna.m.tolle@gmail.com>
2023-08-16 20:05:45 +0000

    fix: several bugs reg. array dimensions; add: plotting option

--- cpm/cpm.py
+++ cpm/cpm.py
@@ -1,8 +1,9 @@
 import numpy as np
 from scipy.stats import pearsonr 
 from sklearn.linear_model import LinearRegression
+import matplotlib.pyplot as plt
 
-def predict(fcs, y):
+def predict(fcs, y, plot_result = False):
     """
     Predicts the response variable y from functional connectivity matrices fcs using connectome-based
     predictive modelling (Shen, X. et al., 2017; Amico and Goni, 2018).
@@ -15,19 +16,20 @@ def predict(fcs, y):
     
     y : vector array 
         response variable that may be predicted from functional connectivity (e.g. cognitive score)
+        
+    plot_result : boolean
+                  if True, produces a scatterplot at the end with predictions versus y
 
     Returns
     -------
-    result : PearsonRResult
-             Pearson correlation object of predicted versus and response variable
-             result.statistic = correlation coefficient
-             result.pvalue = p-value corresponding to correlation coefficient
+    result : y_pred
+             predicted labels
     """
     
     allsubs = np.arange(0, len(y), 1)   
     y_pred = np.zeros(len(y))
     for sub in allsubs:
-        
+                
         # generate subject indices for implementing leave-one-out cross-validation
         trainsubs = allsubs[allsubs !=sub]
         
@@ -37,17 +39,26 @@ def predict(fcs, y):
             
             # compute correlation between FC value and y for all but one subject
             res = pearsonr(fcs[edge, trainsubs], y[trainsubs])
-            
-            # edges with significant positive (p<0.01) correlation are summed into a single subject value ("strength")
-            if res.pvalue < 0.01 && res.statistic > 0:
-                strength += fcs[edge, :].flatten()
-                
+
+            # edges with sign. positive (p<0.01) correlation are summed into a single subject value ("strength")
+            if res.pvalue < 0.01 and res.statistic > 0:
+                 strength += fcs[edge, :]
+                    
         # fit linear model and predict y of left-out subject from strength
-        reg = LinearRegression().fit(strength[trainsubs], y[trainsubs])
-        y_pred(sub) = reg.predict(np.array([[strength(sub)]]))
-        
+        x = np.expand_dims(strength[trainsubs], axis=1)
+        reg = LinearRegression().fit(x, y[trainsubs])
+        y_pred[sub] = reg.predict(np.array([[strength[sub]]]))
+
     # compare predicted with true response
-    return pearsonr(y_pred, y)
+    corr_result = pearsonr(y_pred, y)
+    
+    if plot_result:
+        plt.scatter(y, y_pred);
+        plt.xlabel("true label");
+        plt.ylabel("prediction");
+        plt.title(f"r = {np.round(corr_result.statistic, 2)}, p = {np.round(corr_result.pvalue, 4)}");
+    
+    return y_pred
     
         
     
