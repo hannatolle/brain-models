b99c9eb4a82a81f09949c88c201c76a74efaffb0
Hanna Tolle <hanna.m.tolle@gmail.com>
2023-08-17 23:22:16 +0000

    add: missing package import

--- fmri-synth/fMRIsynth/__init__.py
+++ fmri-synth/fMRIsynth/__init__.py
@@ -1,5 +1,6 @@
 import matplotlib.pyplot as plt
 import numpy as np
 import fastdmf as dmf
+from ks_metric import ks_score
 from bayes_opt import BayesianOptimization, UtilityFunction
 from nilearn.connectome import ConnectivityMeasure
